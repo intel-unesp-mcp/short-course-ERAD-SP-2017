7b322b80b8c742ef1d3ab78d231bcaf228d18c0b
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2017-04-07 10:45:27 -0300

    Add files via upload

---
+++ talk/helloWorldOffload.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("Host have %d logical cores.\n", sysconf(_SC_NPROCESSORS_ONLN)); 
+    fflush(0);
+
+    #pragma offload target(mic)
+    {
+        printf("Co-Processor have %ld logical cores.\n",
+                sysconf(_SC_NPROCESSORS_ONLN )); 
+        fflush(0);
+    }
+
+    #pragma offload target(mic:1)
+    {
+        printf("Co-Processor have %ld logical cores.\n",
+                sysconf(_SC_NPROCESSORS_ONLN )); 
+        fflush(0);
+    }
+
+
+}
